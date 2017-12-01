da68bbd50860d0e55c2a86a7ac596215b775c2b1
Le Quoc Tuan <tuanlq@vega.com.vn>
2017-12-01 09:47:35 +0700

    add permission and some code.

--- app/src/main/java/com/sieudaochichcode/com/qrcodedemo/MainActivity.java
+++ app/src/main/java/com/sieudaochichcode/com/qrcodedemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.sieudaochichcode.com.qrcodedemo;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,13 +13,17 @@ import android.widget.ImageView;
 import com.github.sumimakito.awesomeqr.AwesomeQRCode;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
-public class MainActivity extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
 
     ImageView imageView;
-    Button button;
+    Button button, buttonScan;
     EditText editText;
     String EditTextValue;
 
@@ -32,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,5 +69,49 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QrScanner(view);
+            }
+        });
+    }
+
+    public void QrScanner(View view){
+
+
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();         // Start camera
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mScannerView != null) {
+            mScannerView.stopCamera();           // Stop camera on pause
+        }
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
     }
 }
